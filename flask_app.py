0189e2bcf8f16d11e96d1f507f43693507244c9c
Dennis Horte <dennis@pinterest.com>
2019-11-05 22:16:04 -0800

    Send the message async to prevent slash command from displaying
    If sending the new text directly in the response, the slash command shows up in the channel. To prevent that, send the response asynchronously.
    Also, clean up the code a lot.

--- flask_app.py
+++ flask_app.py
@@ -2,19 +2,27 @@ import random
 import requests
 
 from flask import Flask
-from flask import jsonify
 from flask import request
 
 app = Flask(__name__)
+
+BEARER_TOKEN = None
 
 @app.route('/')
 def hello_world():
     return 'Hello from Flask!'
 
 
-def parse_dice(dice_string):
+def parse_dice_text(dice_text):
+    """
+    Returns a tuple of 3 ints.
+      0: the number of dice
+      1: the size of each die (eg. number of faces)
+      2: the modifier to apply to the die (eg. +2 or -3 or 0)
+    """
+
     # Extract the number of dice from the string, defaulting to 0 if not specified.
-    num_dice, remainder = dice_string.split('d')
+    num_dice, remainder = dice_text.split('d')
     if not num_dice:
         num_dice = 1
     else:
@@ -37,39 +45,59 @@ def parse_dice(dice_string):
     return num_dice, die_size, modifier
 
 
-def get_dice_values(num, size):
+def parse_dice_args(dice_args):
+    """
+    Returns a tuple of two string, the dice part (eg. 2d6+2), and a possibly
+    empty arbitrary message.
+    """
+    args = dice_args.strip().split(' ', 1)
+    if len(args) == 1:
+        args.append('')
+    return args
+
+def get_dice_rolls(num, size):
     rolls = []
     for i in range(num):
         rolls.append(random.randint(1, size))
     return rolls
 
 
-def formatted_user_id(user_id):
-    return '<@{}>'.format(user_id)
+def send_die_roll_message(channel_id, user_id, dice_args):
+    """
+    Channel and username should be in unescaped format.
+      eg. just U1234 or #C1234, and NOT <@U1234|user> <#C1234|general>
+    """
+    dice_text, message = parse_dice_args(dice_args)
+    num_dice, die_size, modifier = parse_dice_text(dice_text)
+    dice_rolls = get_dice_rolls(num_dice, die_size)
+    message = '<@{}> got *{}* on {}'.format(
+        user_id,
+        sum(dice_rolls) + modifier,
+        dice_text,
+    )
 
+    headers = {
+        'Content-Type': "application/json",
+        'Authorization': "Bearer {}".format(BEARER_TOKEN)
+    }
+
+    message_response = requests.post(
+        'https://slack.com/api/chat.postMessage',
+        headers=headers,
+        json={
+            'channel': channel_id,
+            'text': message,
+        }
+    )
+
+    print(message_response.__dict__)
 
 @app.route('/slack/rpg/dice/', methods=['POST'])
 def roll_dice():
-    data = request.form
-    print('----------------------------------------------')
-    print(data)
-
-    args = request.form['text'].strip()
-    args = args.split(' ', 1)
-    dice_string = args[0]
-
-    num_dice, die_size, modifier = parse_dice(dice_string)
-    result = get_dice_values(num_dice, die_size)
-
-    message = '{} rolled {} and got *{}* _rolls: {}_'.format(
-        formatted_user_id(request.form['user_id']),
-        dice_string,
-        sum(result) + modifier,
-        result
+    send_die_roll_message(
+        request.form.get('channel_id'),
+        request.form.get('user_id'),
+        request.form.get('text'),
     )
 
-    return jsonify(
-        success=True,
-        response_type="in_channel",
-        text=message,
-    )
+    return '', 200
